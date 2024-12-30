b83ccba3cc704231e7f50755b02536ab9bf18928
VietIS Anhltv <anhltv@vietis.com.vn>
2024-12-30 10:25:15 +0900

    add responsive and icons

---
+++ main.js
@@ -0,0 +1,10 @@
+const iconBtn = document.querySelector("header__toggle")
+const navMenu = document.querySelector(".header-nav")
+
+iconBtn.addEventListener("click", ()->
+navMenu.classList.toggle("active")
+)
+
+document.querySelectorAll("header-nav__lnk").forEach(n => n.addEventListener("click", () =>{
+    navMenu.classList.remove("active")
+}))
